bb9326aa3f3bc45e8939d93f38dd5cf64bf37207
HuynhVietHoang <102209757+HuynhVietHoang@users.noreply.github.com>
2022-05-08 21:05:17 +0700

    .

---
+++ app/src/main/java/com/example/doanandroid/lietkesp.java
@@ -0,0 +1,14 @@
+package com.example.doanandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lietkesp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
